ebf49ddcdbf99dae812e42a10dfa92d38db3ccc1
seanNCTU <sean19960914.gmail.com>
2018-07-04 00:02:08 +0800

    add path marker

--- catkin_ws/src/visualization/src/visual_path.py
+++ catkin_ws/src/visualization/src/visual_path.py
@@ -11,28 +11,28 @@ class VisualPath(object):
 	def __init__(self):
 		self.node_name = rospy.get_name()
 		self.pub_path = rospy.Publisher("visual/fusion_path", Path, queue_size = 20)
-		#self.pub_marker = rospy.Publisher("visual/path_marker", Marker, queue_size = 20)
+		self.pub_marker = rospy.Publisher("visual/path_marker", Marker, queue_size = 20)
 		self.sub_odom = rospy.Subscriber("/odometry/filtered", Odometry, self.cb_odom, queue_size = 20)
 		self.path = Path()
-		#self.marker = Marker()
-		#self.marker.type = Marker.POINTS
-		#self.marker.action = Marker.ADD
-		#self.marker.scale = Vector3(0.01, 0.01, 0.01)
+		self.marker = Marker()
+		self.marker.type = Marker.POINTS
+		self.marker.action = Marker.ADD
+		self.marker.scale = Vector3(0.01, 0.01, 0.01)
 		rospy.loginfo("[%s] Initialized ..." %(self.node_name))
 
 	def cb_odom(self, msg):
 		self.path.header = msg.header
-		#self.marker.header = msg.header
-		#point_ = Point()
-		#point_ = msg.pose.pose.position
-		#self.marker.points.append(point_)
-		#self.marker.colors.append(ColorRGBA(0.5, 0.0, 1.0, 1.0))
+		self.marker.header = msg.header
+		point_ = Point()
+		point_ = msg.pose.pose.position
+		self.marker.points.append(point_)
+		self.marker.colors.append(ColorRGBA(0.5, 0.0, 1.0, 1.0))
 		pose_ = PoseStamped()
 		pose_.header = msg.header
 		pose_.pose = msg.pose.pose
 		self.path.poses.append(pose_)
 		self.pub_path.publish(self.path)
-		#self.pub_marker.publish(self.marker)
+		self.pub_marker.publish(self.marker)
 
 	
 if __name__ == "__main__":

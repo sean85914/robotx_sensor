7f8ac8ff852ad607ac1486a51247acb6e8ce26c2
seanNCTU <sean19960914.gmail.com>
2018-07-04 22:53:32 +0800

    heading remap

--- catkin_ws/src/localization/src/heading_remap.py
+++ catkin_ws/src/localization/src/heading_remap.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # heading is the angle between the sensor x-axis ang Earth North axis
-# if start from sensor x-axis, then the rotation is position if CCW
+# if start from sensor x-axis, then the rotation is positive if CCW
 # range between -180 and 180 degrees
 
 import rospy
@@ -11,19 +11,27 @@ from math import degrees
 from sensor_msgs.msg import Imu
 
 def cb_imu(msg):
+	imu_remap = Imu()
+	imu_remap = msg
 	quat = [msg.orientation.x, \
 		msg.orientation.y, \
 		msg.orientation.z, \
 		msg.orientation.w]
 	euler = tf.transformations.euler_from_quaternion(quat)
-	heading_rad = euler[2]
+	yaw = euler[2] * -1
+	quat_remap = tf.transformations.quaternion_from_euler(euler[0], euler[1], yaw)
+	imu_remap.orientation = quat_remap
+	pub_imu.publish(imu_remap)
+	'''heading_rad = euler[2]
 	heading_deg = degrees(heading_rad)
 	# Convert to branch [0, 360)
 	if heading_deg < 0:
-		heading_deg += 360
-	print "IMU heading: ", heading_deg 
+		heading_deg += 360	
+	print "IMU heading: ", heading_deg '''
+	print degrees(euler[0]), degrees(euler[1]), degrees(yaw)
 
 if __name__ == "__main__":
 	rospy.init_node('view_heading_node')
+	pub_imu = rospy.Publisher('/imu/data_corrected', Imu, queue_size = 20)
 	rospy.Subscriber('/imu/data', Imu, cb_imu, queue_size = 20)
 	rospy.spin()
